ca85d98d59fa4a7cd4c32173bd2751f57daf9b04
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-21 16:48:51 -0300

    initial commit

---
+++ src/test/java/meli/bootcamp/dentinhos/DentinhosApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.dentinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentinhosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
