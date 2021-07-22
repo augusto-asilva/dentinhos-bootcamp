d86a530cbcaeea03404287d409b95cf37a404de2
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-22 12:52:01 -0300

    database configuration

--- src/main/resources/dentinhos.sql
+++ src/main/resources/dentinhos.sql
@@ -4,6 +4,12 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+
+-- create user
+CREATE USER IF NOT EXISTS 'dentinhos_dba'@'localhost' IDENTIFIED BY 'dentinhos123';
+
+GRANT ALL ON dentinhos.* TO 'dentinhos_dba'@'localhost';
+
 -- -----------------------------------------------------
 -- Schema dentinhos
 -- -----------------------------------------------------
