ce4b0a42aa532e9d8fc69d98de6843814c84cd21
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-22 17:20:29 -0300

    fix: change domain classes and fields

--- src/main/java/meli/bootcamp/dentinhos/domain/Dentist.java
+++ src/main/java/meli/bootcamp/dentinhos/domain/Dentist.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "dentists")
 public class Dentist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/meli/bootcamp/dentinhos/domain/Diary.java
+++ src/main/java/meli/bootcamp/dentinhos/domain/Diary.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "diaries")
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
